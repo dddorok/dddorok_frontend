d504cb4bdeab0bb5b76641bcf9aec35eb689d1fa
byun.sumi <sumi@adenasoft.com>
2025-04-14 14:50:02 +0900

    chore: react query, motion 세팅

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,15 +1,9 @@
-import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { QueryProvider } from "@/lib/react-query";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
