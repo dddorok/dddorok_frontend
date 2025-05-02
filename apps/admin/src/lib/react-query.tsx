e2531d14f066a1e0e80e6f6a5a36941c9c8683a0
byun.sumi <sumi@adenasoft.com>
2025-05-02 11:53:30 +0900

    chore: react query devtools 추가

--- apps/admin/src/lib/react-query.tsx
+++ apps/admin/src/lib/react-query.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,6 +14,9 @@ export const queryClient = new QueryClient({
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
