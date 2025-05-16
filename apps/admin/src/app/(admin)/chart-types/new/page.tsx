54fc60fc8be3cb3882d17bb4fa8be2de69d3e1a0
sumi-0011 <selina2000@naver.com>
2025-05-16 18:05:54 +0900

    fix: error handling fix

--- apps/admin/src/app/(admin)/chart-types/new/page.tsx
+++ apps/admin/src/app/(admin)/chart-types/new/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { Suspense, useState } from "react";
 
 import InformationForm from "./information-form";
 import SvgMappingForm from "./svg-mapping-form";
 
+import { toast } from "@/hooks/use-toast";
 import { measurementRuleQueries } from "@/queries/measurement-rule";
 import { createChartType, uploadSvg } from "@/services/chart-type";
 
@@ -20,29 +22,43 @@ type FormDataType = {
 export default function NewChartTypePage() {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState<FormDataType | null>(null);
-
+  const router = useRouter();
   const onSubmit = async (data: any) => {
     if (!formData || !data) return;
 
     if (!data.file) return;
 
-    const resourceId = await uploadSvg(data.file);
+    try {
+      const resourceId = await uploadSvg(data.file);
 
-    await createChartType({
-      category_large: "의류",
-      category_medium: "상의",
-      section: formData.type === "몸판" ? "BODY" : "SLEEVE",
-      detail_type: formData.detailType,
-      name: formData.chartName,
-      measurement_rule_id: formData.measurementRule ?? "",
-      measurement_code_maps: data.paths.map(
-        (item: { path: { id: string }; selectedMeasurement: string }) => ({
-          measurement_code: item.selectedMeasurement,
-          path_id: item.path.id,
-        })
-      ),
-      resource_id: resourceId,
-    });
+      await createChartType({
+        category_large: "의류",
+        category_medium: "상의",
+        section: formData.type === "몸판" ? "BODY" : "SLEEVE",
+        detail_type: formData.detailType,
+        name: formData.chartName,
+        measurement_rule_id: formData.measurementRule ?? "",
+        measurement_code_maps: data.paths.map(
+          (item: { path: { id: string }; selectedMeasurement: string }) => ({
+            measurement_code: item.selectedMeasurement,
+            path_id: item.path.id,
+          })
+        ),
+        resource_id: resourceId,
+      });
+
+      toast({
+        title: "차트 타입 생성 완료",
+      });
+      router.push(`/chart-types`);
+    } catch (error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        title: "차트 타입 생성 실패",
+        description: error instanceof Error ? error.message : "알 수 없는 오류",
+      });
+    }
   };
 
   return (
