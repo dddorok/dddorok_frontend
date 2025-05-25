c8ae0cdff14185e5396f4d6ead1ca712b19e37f6
sumi-0011 <selina2000@naver.com>
2025-05-25 15:33:57 +0900

    feat: typo setting

---
+++ apps/web/style/typo.ts
@@ -0,0 +1,129 @@
+export const fontSize: Record<
+  string,
+  [string, { lineHeight?: string; letterSpacing?: string; fontWeight?: string }]
+> = {
+  // h1 - Pretendard Extra Bold 48px
+  h1: [
+    "48px",
+    {
+      lineHeight: "1em",
+      letterSpacing: "-1.2%",
+      fontWeight: "800",
+    },
+  ],
+  // h2 - Pretendard SemiBold 30px
+  h2: [
+    "30px",
+    {
+      lineHeight: "1.2em",
+      letterSpacing: "-0.75%",
+      fontWeight: "600",
+    },
+  ],
+  // h3 - Pretendard SemiBold 24px
+  h3: [
+    "24px",
+    {
+      lineHeight: "1.333em",
+      letterSpacing: "-0.6%",
+      fontWeight: "600",
+    },
+  ],
+  // h3-m - Pretendard Medium 24px
+  "h3-m": [
+    "24px",
+    {
+      lineHeight: "1.333em",
+      fontWeight: "500",
+    },
+  ],
+  // h4 - Pretendard SemiBold 20px
+  h4: [
+    "20px",
+    {
+      lineHeight: "1.4em",
+      letterSpacing: "-0.5%",
+      fontWeight: "600",
+    },
+  ],
+  // body/paragraph - Pretendard Regular 16px
+  body: [
+    "16px",
+    {
+      lineHeight: "1.75em",
+      fontWeight: "400",
+    },
+  ],
+  // lead - Pretendard Medium 20px
+  lead: [
+    "20px",
+    {
+      lineHeight: "1.4em",
+      fontWeight: "500",
+    },
+  ],
+  // large - Pretendard SemiBold 18px
+  large: [
+    "18px",
+    {
+      lineHeight: "1.556em",
+      fontWeight: "600",
+    },
+  ],
+  // medium-b - Pretendard Bold 16px
+  "medium-b": [
+    "16px",
+    {
+      lineHeight: "1.5em",
+      fontWeight: "700",
+    },
+  ],
+  // medium-sb - Pretendard SemiBold 16px
+  "medium-sb": [
+    "16px",
+    {
+      lineHeight: "1.5em",
+      fontWeight: "600",
+    },
+  ],
+  // medium - Pretendard Medium 16px
+  medium: [
+    "16px",
+    {
+      lineHeight: "1.5em",
+      fontWeight: "500",
+    },
+  ],
+  // medium-r - Pretendard Regular 16px
+  "medium-r": [
+    "16px",
+    {
+      lineHeight: "1.5em",
+      fontWeight: "400",
+    },
+  ],
+  // small - Pretendard Medium 14px
+  small: [
+    "14px",
+    {
+      lineHeight: "1em",
+      fontWeight: "500",
+    },
+  ],
+  // xsmall - Pretendard Regular 12px
+  xsmall: [
+    "12px",
+    {
+      lineHeight: "1.167em",
+      fontWeight: "400",
+    },
+  ],
+  // subtle - Pretendard Regular 14px
+  subtle: [
+    "14px",
+    {
+      lineHeight: "1.429em",
+      fontWeight: "400",
+    },
+  ],
+};
