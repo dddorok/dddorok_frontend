5fcc24b956377f020f5d7921e3c37b9258937e29
sumi-0011 <selina2000@naver.com>
2025-05-26 14:46:43 +0900

    fix: utils 수정

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,50 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { extendTailwindMerge } from "tailwind-merge";
+
+import { colors } from "../../style/color";
+import { fontSize } from "../../style/typo";
+
+type FontSizeStyle = {
+  fontWeight?: string;
+  lineHeight?: string;
+  letterSpacing?: string;
+};
+
+// typo.ts에서 폰트 크기와 폰트 웨이트 키 추출
+const fontSizes = Object.keys(fontSize);
+const fontWeights = Array.from(
+  new Set(
+    Object.values(fontSize)
+      .map(([_, style]: [string, FontSizeStyle]) => style.fontWeight)
+      .filter((weight): weight is string => weight !== undefined)
+  )
+);
+
+// color.ts에서 색상 키 추출
+const colorKeys = Object.entries(colors).reduce<string[]>(
+  (acc, [category, values]) => {
+    if (values && typeof values === "object") {
+      Object.keys(values).forEach((key) => {
+        acc.push(`${category}-${key}`);
+      });
+    }
+    return acc;
+  },
+  []
+);
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [{ text: fontSizes }],
+      "font-weight": [{ font: fontWeights }],
+      "text-color": [{ text: colorKeys }],
+      "bg-color": [{ bg: colorKeys }],
+      "border-color": [{ border: colorKeys }],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
