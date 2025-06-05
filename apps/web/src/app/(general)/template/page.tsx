20839b1896f3a91c1505b536d6c5d26e39651867
sumi-0011 <selina2000@naver.com>
2025-06-05 17:14:40 +0900

    feat: template list

--- apps/web/src/app/(general)/template/page.tsx
+++ apps/web/src/app/(general)/template/page.tsx
@@ -4,9 +4,6 @@ import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { useState } from "react";
 
-import { CarouselSpacing } from "./_components/Slider";
-
-import Header from "@/components/layout/Header";
 import CommonLayout from "@/components/layout/MainLayout";
 import { ROUTE } from "@/constants/route";
 import { cn } from "@/lib/utils";
@@ -23,7 +20,7 @@ export default function TemplateListPage() {
   console.log(templateList);
   return (
     <CommonLayout>
-      <div className="pt-16 px-8 pb-12">
+      <div className="py-16 px-8">
         <div className="flex items-center justify-between gap-4 flex-col">
           <h2 className="text-[32px] font-semibold text-neutral-1000">
             템플릿으로 뜨개 도안을 만들어보세요.
@@ -33,15 +30,17 @@ export default function TemplateListPage() {
           </p>
         </div>
       </div>
-      <div className="mt-1 border-t border-[0.5px] border-neutralAlpha-NA20 py-6">
+      <hr className="mb-6 w-screen absolute left-1/2 border-none -translate-x-1/2 h-[0.5px] bg-neutralAlpha-NA20" />
+      <div className="py-6">
         <div className="flex items-center justify-center gap-5">
           {TAB.map((tab) => (
             <button
               key={tab}
               onClick={() => setActiveTab(tab)}
               className={cn(
-                "text-h3 px-5 py-3 text-primary-PR text-[24px] font-semibold",
-                activeTab === tab && "bg-primary-PR text-neutral-N0 rounded-xl"
+                "px-4 py-[6px] text-neutral-N500 text-medium-r rounded-[6px] border border-neutral-N400",
+                activeTab === tab &&
+                  "bg-primary-PR text-medium-b text-neutral-N0   border-primary-PR"
               )}
             >
               {tab}
@@ -49,7 +48,7 @@ export default function TemplateListPage() {
           ))}
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-[30px] container pt-12 px-8">
+      <div className="grid grid-cols-4 gap-x-3 gap-y-16 container py-12 ">
         {templateList?.items.map((template) => (
           <Link
             href={ROUTE.PROJECT.NEW(template.id, template.name)}
@@ -60,9 +59,6 @@ export default function TemplateListPage() {
           </Link>
         ))}
       </div>
-      {/* <div className="container pt-12">
-        <CarouselSpacing />
-      </div> */}
     </CommonLayout>
   );
 }
@@ -70,7 +66,7 @@ export default function TemplateListPage() {
 function TemplateItem({ template }: { template: TemplateType }) {
   return (
     <div>
-      <div className="bg-neutral-N200 h-[200px]">
+      <div className="bg-primary-PR100 border border-neutralAlpha-NA05 rounded-lg h-[216px]">
         {template.thumbnail_url && (
           <img
             src={template.thumbnail_url}
@@ -79,7 +75,7 @@ function TemplateItem({ template }: { template: TemplateType }) {
           />
         )}
       </div>
-      <p className="text-[21px] text-neutral-N800 font-medium py-2 truncate">
+      <p className="text-medium text-neutral-N800 mt-[9px] truncate">
         {template.name}
       </p>
     </div>
