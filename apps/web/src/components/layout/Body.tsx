df85dee17b449fdee1aac0b8ae4363567f621270
sumi-0011 <selina2000@naver.com>
2025-06-05 16:01:01 +0900

    feat: mypage

--- apps/web/src/components/layout/Body.tsx
+++ apps/web/src/components/layout/Body.tsx
@@ -1,3 +1,3 @@
 export default function Body({ children }: { children: React.ReactNode }) {
-  return <div className="flex-1 container">{children}</div>;
+  return <div className="flex-1 container px-8 pt-16 pb-20">{children}</div>;
 }
