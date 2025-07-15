0ba32fd5dead9a505b26f7d7c731d7430feeb564
sumi-0011 <selina2000@naver.com>
2025-07-15 20:27:24 +0900

    fix: window origin으로 리다이렉션 url 수정

--- apps/web/src/app/(auth)/auth/_components/LoginForm.tsx
+++ apps/web/src/app/(auth)/auth/_components/LoginForm.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
-import { createSession, createTestSession } from "@/lib/auth";
+import { createSession } from "@/lib/auth";
 import { apiInstance } from "@/services/instance";
 
 /* eslint-disable @next/next/no-img-element */
@@ -9,6 +10,15 @@ export function LoginForm(props: {
   onClick?: (url: string) => void;
   type: "join" | "login";
 }) {
+  const [origin, setOrigin] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!window) return;
+    setOrigin(window.origin);
+  }, []);
+
+  if (!origin) return null;
+
   return (
     <div className="flex flex-col gap-4">
       <LoginButton
@@ -19,6 +29,7 @@ export function LoginForm(props: {
             ? AUTH_PROVIDERS.NAVER.joinText
             : AUTH_PROVIDERS.NAVER.loginText
         }
+        authUrl={AUTH_PROVIDERS.NAVER.authUrl(`${origin}/oauth/redirect`)}
       />
       <LoginButton
         {...AUTH_PROVIDERS.GOOGLE}
@@ -28,6 +39,7 @@ export function LoginForm(props: {
             ? AUTH_PROVIDERS.GOOGLE.joinText
             : AUTH_PROVIDERS.GOOGLE.loginText
         }
+        authUrl={AUTH_PROVIDERS.GOOGLE.authUrl(`${origin}/oauth/redirect`)}
       />
       <LoginButton
         {...AUTH_PROVIDERS.KAKAO}
@@ -37,6 +49,7 @@ export function LoginForm(props: {
             ? AUTH_PROVIDERS.KAKAO.joinText
             : AUTH_PROVIDERS.KAKAO.loginText
         }
+        authUrl={AUTH_PROVIDERS.KAKAO.authUrl(`${origin}/oauth/redirect`)}
       />
       <TestLoginButton />
     </div>
@@ -106,24 +119,23 @@ const AUTH_PROVIDERS = {
     imageSrc: "/images/login/naver.svg",
     joinText: "네이버로 가입하기",
     loginText: "네이버 로그인",
-    authUrl: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI}?provider=naver`,
+    authUrl: (redirectUri: string) =>
+      `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${redirectUri}?provider=naver`,
   },
   GOOGLE: {
     name: "구글",
     imageSrc: "/images/login/google.png",
     joinText: "구글로 가입하기",
     loginText: "구글 로그인",
-    authUrl: `https://accounts.google.com/o/oauth2/v2/auth?
-            client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
-            &redirect_uri=${process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI}?provider=google
-            &response_type=code
-            &scope=email profile`,
+    authUrl: (redirectUri: string) =>
+      `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}?provider=google&response_type=code&scope=email profile`,
   },
   KAKAO: {
     name: "카카오",
     imageSrc: "/images/login/kakao.png",
     joinText: "카카오로 가입하기",
     loginText: "카카오 로그인",
-    authUrl: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI}?provider=kakao`,
+    authUrl: (redirectUri: string) =>
+      `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${redirectUri}?provider=kakao`,
   },
 };
